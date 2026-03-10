9bab7b513a27a974e308e49609d37401f505e0e2
Amir <60442490+amirwolf5122@users.noreply.github.com>
2026-03-10 20:20:07 +0330

    Fix Error from_rank

--- telethon_up/__init__.py
+++ telethon_up/__init__.py
@@ -127,12 +127,11 @@ def check():
                         target_api = os.path.join(folder, "telethon_generator", "data", "api.tl")
                         os.makedirs(os.path.dirname(target_api), exist_ok=True)
                         shutil.copy(api_tl, target_api)
-                        #Fix error from_rank
+                        #Fix Error from_rank
                         #===
                         with open(target_api, 'r', encoding='utf-8') as f:
                             content = f.read()
-                        content = re.sub(r'from_rank:flags\d+\.\d+\?int', '', content)
-                        content = re.sub(r'\?int from_rank:flags\d+\.\d+', '', content)
+                        content = re.sub(r'int from_rank:flags.*\?', '', content)
                         with open(target_api, 'w', encoding='utf-8') as f:
                             f.write(content)
                         #===
